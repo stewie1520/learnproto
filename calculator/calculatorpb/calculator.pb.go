1c52e20a82613cb61d8d8ffa792b42a00bc100f9
Stewie <41299755+stewie1520@users.noreply.github.com>
2021-10-02 11:25:33 +0700

    fix
    Signed-off-by: Stewie <41299755+stewie1520@users.noreply.github.com>

--- calculator/calculatorpb/calculator.pb.go
+++ calculator/calculatorpb/calculator.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.27.1
 // 	protoc        v3.18.0
-// source: calculator/calculatorpb/calculator.proto
+// source: calculatorpb/calculator.proto
 
 package calculatorpb
 
@@ -21,20 +21,19 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-var File_calculator_calculatorpb_calculator_proto protoreflect.FileDescriptor
+var File_calculatorpb_calculator_proto protoreflect.FileDescriptor
 
-var file_calculator_calculatorpb_calculator_proto_rawDesc = []byte{
-	0x0a, 0x28, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2f, 0x63, 0x61, 0x6c,
-	0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x70, 0x62, 0x2f, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c,
-	0x61, 0x74, 0x6f, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0a, 0x63, 0x61, 0x6c, 0x63,
-	0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x32, 0x13, 0x0a, 0x11, 0x43, 0x61, 0x6c, 0x63, 0x75, 0x6c,
-	0x61, 0x74, 0x6f, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x42, 0x1b, 0x5a, 0x19, 0x2e,
-	0x2f, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2f, 0x63, 0x61, 0x6c, 0x63,
-	0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_calculatorpb_calculator_proto_rawDesc = []byte{
+	0x0a, 0x1d, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x70, 0x62, 0x2f, 0x63,
+	0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
+	0x0a, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x32, 0x13, 0x0a, 0x11, 0x43,
+	0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65,
+	0x42, 0x10, 0x5a, 0x0e, 0x2e, 0x2f, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72,
+	0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
-var file_calculator_calculatorpb_calculator_proto_goTypes = []interface{}{}
-var file_calculator_calculatorpb_calculator_proto_depIdxs = []int32{
+var file_calculatorpb_calculator_proto_goTypes = []interface{}{}
+var file_calculatorpb_calculator_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -42,28 +41,28 @@ var file_calculator_calculatorpb_calculator_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_calculator_calculatorpb_calculator_proto_init() }
-func file_calculator_calculatorpb_calculator_proto_init() {
-	if File_calculator_calculatorpb_calculator_proto != nil {
+func init() { file_calculatorpb_calculator_proto_init() }
+func file_calculatorpb_calculator_proto_init() {
+	if File_calculatorpb_calculator_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_calculator_calculatorpb_calculator_proto_rawDesc,
+			RawDescriptor: file_calculatorpb_calculator_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   0,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_calculator_calculatorpb_calculator_proto_goTypes,
-		DependencyIndexes: file_calculator_calculatorpb_calculator_proto_depIdxs,
+		GoTypes:           file_calculatorpb_calculator_proto_goTypes,
+		DependencyIndexes: file_calculatorpb_calculator_proto_depIdxs,
 	}.Build()
-	File_calculator_calculatorpb_calculator_proto = out.File
-	file_calculator_calculatorpb_calculator_proto_rawDesc = nil
-	file_calculator_calculatorpb_calculator_proto_goTypes = nil
-	file_calculator_calculatorpb_calculator_proto_depIdxs = nil
+	File_calculatorpb_calculator_proto = out.File
+	file_calculatorpb_calculator_proto_rawDesc = nil
+	file_calculatorpb_calculator_proto_goTypes = nil
+	file_calculatorpb_calculator_proto_depIdxs = nil
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -105,5 +104,5 @@ var _CalculatorService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*CalculatorServiceServer)(nil),
 	Methods:     []grpc.MethodDesc{},
 	Streams:     []grpc.StreamDesc{},
-	Metadata:    "calculator/calculatorpb/calculator.proto",
+	Metadata:    "calculatorpb/calculator.proto",
 }
